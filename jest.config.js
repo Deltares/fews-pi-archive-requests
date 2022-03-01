8211aeb4727005eed91f4dd50ec4e093d822fa96
grijzea <andre.grijze@deltares.nl>
2022-03-01 15:15:58 +0100

    commit

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,6 @@ module.exports = {
     testRegex: "\.spec\.(ts|tsx|js)$",
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     reporters: [ "default", "jest-junit" ],
+    "testResultsProcessor": "jest-teamcity-reporter",
+    "coverageReporters": ["lcov", "text", "teamcity"]
 }
